fb5b9da7e2ac5559ebda737a606560633332f0ae
sanketss0864 <103756320+sanketss0864@users.noreply.github.com>
2024-06-15 12:12:17 +0530

    update

--- day9/home-decor/js/main.js
+++ day9/home-decor/js/main.js
@@ -1,5 +1,5 @@
 window.onload=()=>{
-    const swiper = new Swiper('.swiper', {
+   new Swiper('.swiper', {
     
         direction: 'horizontal',
         loop: true,
@@ -17,5 +17,77 @@ window.onload=()=>{
         scrollbar: {
           el: '.swiper-scrollbar',
         },
+      });
+     new Swiper('.offerslider', {
+    
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 4,
+        spaceBetween:4 ,
+        // pagination: {
+        //   el: '.swiper-pagination',
+        // },
+      
+        navigation: {
+          nextEl: '#offer-next',
+          prevEl: '#offer-prev',
+        },
+      
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+        breakpoints: {
+        
+          320: {
+            slidesPerView: 2,
+            spaceBetween: 10
+          },
+         
+          480: {
+            slidesPerView: 2,
+            spaceBetween: 10
+          },
+         
+          640: {
+            slidesPerView: 4,
+            spaceBetween: 10
+          }
+        }
+      });
+      new Swiper('.trendslider', {
+    
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 4,
+        spaceBetween:4 ,
+        // pagination: {
+        //   el: '.swiper-pagination',
+        // },
+      
+        navigation: {
+          nextEl: '#trend-next',
+          prevEl: '#trend-prev',
+        },
+      
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+        breakpoints: {
+        
+          320: {
+            slidesPerView: 2,
+            spaceBetween: 5
+          },
+         
+          480: {
+            slidesPerView: 2,
+            spaceBetween: 5
+          },
+         
+          640: {
+            slidesPerView: 4,
+            spaceBetween: 10
+          }
+        }
       });
 }
